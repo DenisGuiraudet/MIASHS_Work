0c185a9189839295a60b8bcec377a1617857c647
Denis_G <guiraudet@live.fr>
2018-02-09 16:46:07 +0100

    push

--- MIASHS/Web Framework/angular1/js.js
+++ MIASHS/Web Framework/angular1/js.js
@@ -1,25 +1,43 @@
 
+var 
+
+angular.module('marmotte', [])
+  .controller('marmotteListController' ,function() {
+
+    var marmotteList = this;
+
+
+
+  });
+
+
 window.onload = function(){
 
   let cards = document.querySelectorAll(".card");
   for (let card of cards) {
-    let back = document.createElement("div");
-    back.classList.add("back");
-    back.classList.add("position-absolute");
-    back.classList.add("border");
-    back.classList.add("rounded");
-      let backImg = document.createElement("img");
-      backImg.classList.add("card-img-top");
-      backImg.src = "https://cdn.vox-cdn.com/thumbor/O_m3r1bT2CtzOHY-9cwDcL4lCgQ=/0x0:1920x1080/1200x800/filters:focal(807x387:1113x693)/cdn.vox-cdn.com/uploads/chorus_image/image/53872699/battle_net.0.jpg";
-    back.appendChild(backImg);
-      let backDiv = document.createElement("div");
-      backDiv.classList.add("card-footer");
-        let backDivH = document.createElement("h5");
-        backDivH.classList.add("card-title");
-        backDivH.appendChild(document.createTextNode("???"));
-      backDiv.appendChild(backDivH);
-    back.appendChild(backDiv);
-    card.appendChild(back);
+    card.appendChild(get_back_side());
   }
+
+}
+
+function get_back_side() {
+
+  let back = document.createElement("div");
+  back.classList.add("back");
+  back.classList.add("position-absolute");
+  back.classList.add("border");
+  back.classList.add("rounded");
+    let backImg = document.createElement("img");
+    backImg.classList.add("card-img-top");
+    backImg.src = "https://cdn.vox-cdn.com/thumbor/O_m3r1bT2CtzOHY-9cwDcL4lCgQ=/0x0:1920x1080/1200x800/filters:focal(807x387:1113x693)/cdn.vox-cdn.com/uploads/chorus_image/image/53872699/battle_net.0.jpg";
+  back.appendChild(backImg);
+    let backDiv = document.createElement("div");
+    backDiv.classList.add("card-footer");
+      let backDivH = document.createElement("h5");
+      backDivH.classList.add("card-title");
+      backDivH.appendChild(document.createTextNode("???"));
+    backDiv.appendChild(backDivH);
+  back.appendChild(backDiv);
+  return back;
 
 }
