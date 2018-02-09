44bad2f71fa627e6aaabb493baa876782de731bb
Denis_G <guiraudet@live.fr>
2018-02-09 17:40:15 +0100

    kdo

--- MIASHS/Web Framework/angular1/db.js
+++
@@ -1,5 +0,0 @@
-db.collection("data").get().then((querySnapshot) => {
-    querySnapshot.forEach((doc) => {
-        console.log(doc.data());
-    });
-});
--- MIASHS/Web Framework/angular1/js.js
+++ MIASHS/Web Framework/angular1/js.js
@@ -1,20 +1,31 @@
 
-var maMarmotte = {
-  id1: {
+var maMarmotte = [
+  {
     name: "OW",
-    img: null,
+    img: "null",
     help: "shooter"
   },
-  id2: {
+  {
     name: "WoW",
-    img: null,
+    img: "null",
     help: "meuporg"
   }
-}
+];
 
 angular.module('marmotte', [])
   .controller('marmotteListController', function($scope) {
 
     $scope.marmotteList = maMarmotte;
+    console.log($scope.marmotteList);
+
+    db.collection("data").get().then((querySnapshot) => {
+        querySnapshot.forEach((doc) => {
+          console.log(doc.data());
+          $scope.marmotteList.push(doc.data());
+          console.log($scope.marmotteList);
+        });
+    });
+
+
 
   });
