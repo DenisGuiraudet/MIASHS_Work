d3e4a4c4fbde97d541229a410b84af4202539dc1
Denis_G <guiraudet@live.fr>
2018-02-10 19:18:32 +0100

    done

--- MIASHS/Web Framework/angular1/js.js
+++ MIASHS/Web Framework/angular1/js.js
@@ -6,10 +6,22 @@ angular.module('marmotte', [])
 
     db.collection("data").get().then((querySnapshot) => {
         querySnapshot.forEach((doc) => {
-          $scope.marmotteList.push(doc.data());
+          $scope.marmotteList.push(doc.data(), doc.data());
+          $scope.marmotteList.sort(function() { return 0.5 - Math.random() });
           $scope.$apply();
         });
     });
 
+    $scope.flipFront = function($event) {
+      $event.currentTarget.style.transform = "rotateY(180deg)";
+      $event.currentTarget.parentElement.querySelector(".back").style.transform = "none";
+
+    }
+
+    $scope.flipBack = function($event) {
+      $event.currentTarget.style.transform = "rotateY(180deg)";
+      $event.currentTarget.parentElement.querySelector(".front").style.transform = "rotateY(360deg)";
+
+    }
 
   });
