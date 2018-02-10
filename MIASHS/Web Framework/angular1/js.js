6383175c28f932d489473399619d38d64ee60c8a
Denis_G <guiraudet@live.fr>
2018-02-10 18:18:42 +0100

    ça marche lel

--- MIASHS/Web Framework/angular1/js.js
+++ MIASHS/Web Framework/angular1/js.js
@@ -1,28 +1,13 @@
 
-var maMarmotte = [
-  {
-    name: "OW",
-    img: "null",
-    help: "shooter"
-  },
-  {
-    name: "WoW",
-    img: "null",
-    help: "meuporg"
-  }
-];
-
 angular.module('marmotte', [])
   .controller('marmotteListController', function($scope) {
 
-    $scope.marmotteList = maMarmotte;
-    console.log($scope.marmotteList);
+    $scope.marmotteList = [];
 
     db.collection("data").get().then((querySnapshot) => {
         querySnapshot.forEach((doc) => {
-          console.log(doc.data());
           $scope.marmotteList.push(doc.data());
-          console.log($scope.marmotteList);
+          $scope.$apply();
         });
     });
 
