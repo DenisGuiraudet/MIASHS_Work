14b3cab5a7ddb6d882e123281b1398f864e1bcd9
Kamideux <matthieu.orriere@gmail.com>
2018-02-12 15:57:07 +0100

    Perroquet

--- MIASHS/Web Framework/angular1/js.js
+++ MIASHS/Web Framework/angular1/js.js
@@ -1,3 +1,7 @@
+var tempCard = null;
+var tempActu = null;
+var waiting = false;
+var nbFlip = 0;
 
 angular.module('marmotte', [])
   .controller('marmotteListController', function($scope) {
@@ -5,6 +9,7 @@ angular.module('marmotte', [])
     $scope.marmotteList = [];
 
     db.collection("data").get().then((querySnapshot) => {
+
         querySnapshot.forEach((doc) => {
           $scope.marmotteList.push(doc.data(), doc.data());
           $scope.marmotteList.sort(function() { return 0.5 - Math.random() });
@@ -12,16 +17,43 @@ angular.module('marmotte', [])
         });
     });
 
-    $scope.flipFront = function($event) {
-      $event.currentTarget.style.transform = "rotateY(180deg)";
-      $event.currentTarget.parentElement.querySelector(".back").style.transform = "none";
-
-    }
-
     $scope.flipBack = function($event) {
-      $event.currentTarget.style.transform = "rotateY(180deg)";
-      $event.currentTarget.parentElement.querySelector(".front").style.transform = "rotateY(360deg)";
-
+      if(waiting == true){
+        return;
+      }
+      waiting = true;
+      tempActu = $event.currentTarget;
+      tempActu.style.transform = "rotateY(180deg)";
+      tempActu.parentElement.querySelector(".front").style.transform = "rotateY(360deg)";
+      if (tempCard == null) {
+        tempCard = tempActu;
+        waiting = false;
+      }
+      else{
+        if (tempCard.parentElement.querySelector(".front h5.card-title").textContent == tempActu.parentElement.querySelector(".front h5.card-title").textContent) {
+          tempCard = null;
+          tempActu = null;
+          waiting = false;
+          nbFlip += 2;
+        }
+        else{
+          setTimeout(function () {
+            tempCard.parentElement.querySelector(".front").style.transform = "rotateY(180deg)";
+            tempCard.parentElement.querySelector(".back").style.transform = "none";
+            tempActu.parentElement.querySelector(".front").style.transform = "rotateY(180deg)";
+            tempActu.parentElement.querySelector(".back").style.transform = "none";
+            tempCard = null;
+            tempActu = null;
+            waiting = false;
+          }, 1000);
+        }
+      }
+      if (nbFlip == $scope.marmotteList.length) {
+        alert("Bravo !\n Denis Steam : Snoopryx \n Matthieu Steam : Kamideus");
+        
+      }
     }
+
+
 
   });
