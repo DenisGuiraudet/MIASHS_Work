1a4a35aa824a030b5ce16badc55f4e47b00de8c5
Denis_G <guiraudet@live.fr>
2018-03-23 16:18:57 +0100

    hs guud

--- MIASHS/Haskell/fifo/liste.hs
+++ MIASHS/Haskell/fifo/liste.hs
@@ -1,22 +1,25 @@
--- :r POUR RECHARGER LE FICHIER !!!!
-newtype FileAttente a = FileAttente [a] deriving Show
 
+newtype FIFO a = FIFO [a] deriving Show
 
-estVide :: FileAttente a -> Bool
-estVide (FileAttente []) = True
-estVide _ = False
+isEmpty :: FIFO a -> Bool
+isEmpty (FIFO []) = True
+isEmpty _ = False
 
-ajouter :: a -> FileAttente a -> FileAttente a
-ajouter x (FileAttente file) = FileAttente (x:file)
+add :: FIFO a -> a -> FIFO a
+add (FIFO file) x = FIFO (x:file)
 
-tete :: FileAttente a -> Maybe a
-tete (FileAttente []) = Nothing
-tete (FileAttente (x:xs)) = Just x
+haed:: FIFO a -> Maybe a
+haed (FIFO []) = Nothing
+haed (FIFO (x:xs)) = Just x
 
-oter :: FileAttente a -> FileAttente a
-oter (FileAttente []) = FileAttente []
-oter (FileAttente (x:xs)) = (FileAttente (xs))
+remove :: FIFO a -> FIFO a
+remove (FIFO []) = FIFO []
+remove (FIFO (x:xs)) = (FIFO (xs))
 
 
 main = do
-  print("bleur")
+  print(isEmpty (FIFO []))
+  print(isEmpty (FIFO [1,2,3]))
+  print(isEmpty (add (FIFO []) 4))
+  print(isEmpty (remove (FIFO [5])))
+  print(haed (FIFO [6,7,8]))
